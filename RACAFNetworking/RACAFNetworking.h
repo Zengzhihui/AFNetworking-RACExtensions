f6534d186e8a555e51511ff7548880161dbb0f6f
Zhang Qigang <zhangqigang@letv.com>
2015-12-18 16:55:37 +0800

    modified

--- RACAFNetworking/RACAFNetworking.h
+++ RACAFNetworking/RACAFNetworking.h
@@ -1,9 +1,20 @@
 //
 //  RACAFNetworking.h
-//  Reactive AFNetworking Example
+//  RACAFNetworking
 //
-//  Created by Robert Widmann on 3/28/13.
-//  Copyright (c) 2013 CodaFi. All rights reserved.
+//  Created by Kerberos Zhang on 15/12/18.
+//  Copyright © 2015年 Robert Widmann. All rights reserved.
 //
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RACAFNetworking.
+FOUNDATION_EXPORT double RACAFNetworkingVersionNumber;
+
+//! Project version string for RACAFNetworking.
+FOUNDATION_EXPORT const unsigned char RACAFNetworkingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RACAFNetworking/PublicHeader.h>
+
 
 #import <RACAFNetworking/AFHTTPSessionManager+RACSupport.h>
